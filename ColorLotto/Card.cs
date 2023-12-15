96aa9c9b2daf7471aa1ed6b41eb8bd98c20c26f5
GET Academy <terje@getacademy.no>
2023-12-15 15:23:34 +0100

    Create Card.cs

---
+++ ColorLotto/Card.cs
@@ -0,0 +1,36 @@
+﻿namespace ColorLotto
+{
+    internal class Card
+    {
+        private bool _isOpen;
+        private ConsoleColor _color;
+        private readonly int _number;
+
+        public Card(int number)
+        {
+            _number = number;
+        }
+
+        public void Open()
+        {
+            _isOpen = true;
+        }
+
+        public void Match(Card otherCard)
+        {
+            if (otherCard._color != _color)
+            {
+                _isOpen = false;
+                otherCard._isOpen = false;
+            }
+        }
+
+        public void Show(int x, int y)
+        {
+            Console.BackgroundColor = _isOpen ? _color : ConsoleColor.Gray;
+            Console.SetCursorPosition(x, y);
+            Console.Write(_number);
+        }
+
+    }
+}
