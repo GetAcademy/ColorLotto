9a4fe522ab044e41e7d4d0422f33a213bb7779d2
GET Academy <terje@getacademy.no>
2023-12-15 15:26:11 +0100

    Create Game.cs

---
+++ ColorLotto/Game.cs
@@ -0,0 +1,14 @@
+﻿namespace ColorLotto
+{
+    internal class Game
+    {
+        /*
+         * Lage en array av cards og initalisere meg forskjellige
+         * Vise kortene i et rutenet
+         * Metode for å velge kort - som kalle Open på det kortet
+         *   - hvis det er andre kort som velges, kalles Match på det første og det andre kortet
+         * Ev. kjenne igjen om spillet ferdig + starte på nytt
+         * Ev. lage random farger
+         */
+    }
+}
